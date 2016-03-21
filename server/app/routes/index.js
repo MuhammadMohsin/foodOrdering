3051fb5bc9191739b61bd3768adef49929124b96
mohsinkhalid <mmohsinkhalid@outlook.com>
2016-03-21 14:13:23 +0500

    moved files to app server

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/rest/api/:service/:method', function(req, res, next) {
-  res.render('index', { title: req.method+' '+ req.url });
+  res.send('index', { title: req.method+' '+ req.url });
 });
 
 router.post('/rest/api/:service/:method', function(req, res, next) {
-  res.render('index', { title: req.method+' '+ req.url });
+  res.send('index', { title: req.method+' '+ req.url });
 });
 
 router.put('/rest/api/:service/:method', function(req, res, next) {
-  res.render('index', { title: req.method+' '+ req.url });
+  res.send('index', { title: req.method+' '+ req.url });
 });
 
 router.delete('/rest/api/:service/:method', function(req, res, next) {
-  res.render('index', { title: req.method+' '+ req.url });
+  res.send('index', { title: req.method+' '+ req.url });
 });
 
 module.exports = router;
